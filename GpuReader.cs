334e2f06ac8ad0416e85dbb3d79f9d8b3f825705
Ibro <ibro@ibro.me>
2022-11-09 19:04:02 +0100

    Fix

--- GpuReader.cs
+++ GpuReader.cs
@@ -1,7 +1,35 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace GpuUsageReader;
 
+[SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
 public class GpuReader
 {
+	public static List<PerformanceCounter> GetGPUCounters()
+	{
+		var category = new PerformanceCounterCategory("GPU Engine");
+		var counterNames = category.GetInstanceNames();
+
+		var gpuCounters = counterNames
+			.Where(counterName => counterName.EndsWith("engtype_3D"))
+			.SelectMany(counterName => category.GetCounters(counterName))
+			.Where(counter => counter.CounterName.Equals("Utilization Percentage"))
+			.ToList();
+            
+		return gpuCounters;
+	}
+    
+	public static async Task<float> GetGPUUsage(List<PerformanceCounter> gpuCounters)
+	{
+		gpuCounters.ForEach(x => x.NextValue());
+
+		await Task.Delay(1000);
+
+		var result = gpuCounters.Sum(x => x.NextValue());
+
+		return result;
+	}
+
 	public static string GetLine()
 	{
 		var data = GetData();
--- Program.cs
+++ Program.cs
@@ -5,19 +5,27 @@ var delay = TimeSpan.FromSeconds(2);
 bool keepLooping = true;
 while (keepLooping)
 {
-	// Read the GPU usage
-	var line = GpuReader.GetLine();
+	// // Read the GPU usage
+	// var line = GpuReader.GetLine();
+	//
+	// // Print out the GPU usage
+	// Console.WriteLine(line);
 	
-	// Print out the GPU usage
-	Console.WriteLine(line);
+	try
+	{
+		var gpuCounters = GpuReader.GetGPUCounters();         
+		var gpuUsage = await GpuReader.GetGPUUsage(gpuCounters);
+		Console.WriteLine(gpuUsage);
+	} catch {}
 
 	
-	// Exit if ESC key is pressed
-	if (Console.ReadKey().Key == ConsoleKey.Escape)
-	{
-		keepLooping = false;
-	}
-	
+	// // Exit if ESC key is pressed
+	// if (Console.ReadKey().Key == ConsoleKey.Escape)
+	// {
+	// 	keepLooping = false;
+	// }
+
+	Console.WriteLine("Waiting....");
 	await Task.Delay(delay);
 }
 
